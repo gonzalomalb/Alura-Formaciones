6a7e20df7c805756f739050e42e0a4c8978c971f
OtzalDev <gonzama.albornoz@gmail.com>
2022-05-05 17:26:26 -0300

    js c1 m1

---
+++ 3_JavaScript/1_Introduccion al lenguaje de la web/M1/js/principal.js
@@ -0,0 +1,5 @@
+var titulo = document.querySelector(".titulo");
+console.log(titulo)
+console.log(titulo.textContent);
+titulo.textContent = "Buena Vida Nutrición";
+console.log("Estoy imprimiendo de una fuente externa que es principal.js");

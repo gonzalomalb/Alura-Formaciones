3b777f71f821ddaef1755499c76cbd326c7cd23f
OtzalDev <gonzama.albornoz@gmail.com>
2022-05-29 04:24:47 -0300

    code

--- 3_JavaScript/2_Consolidando conocimientos JS/M5/js/form.js
+++ 3_JavaScript/2_Consolidando conocimientos JS/M5/js/form.js
@@ -1,15 +1,15 @@
-var botonAdicionar =  document.querySelector("#adicionar-paciente");
+var botonAdicionar = document.querySelector("#adicionar-paciente");
 
-botonAdicionar.addEventListener("click",function(event){
+botonAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
 
     var form = document.querySelector("#form-adicionar");
     var paciente = capturarDatosPaciente(form);
-    
 
-    var errores = validarPaciente(paciente); 
+
+    var errores = validarPaciente(paciente);
     console.log(errores);
-    if(errores.length > 0){
+    if (errores.length > 0) {
         exhibirMensajesErrores(errores);
         return;
     }
@@ -21,77 +21,77 @@ botonAdicionar.addEventListener("click",function(event){
     mensajesErrores.innerHTML = "";
 });
 
-function adicionarPacienteEnLaTabla(paciente){
+function adicionarPacienteEnLaTabla(paciente) {
     var pacienteTr = construirTr(paciente);
     var tabla = document.querySelector("#tabla-pacientes");
     tabla.appendChild(pacienteTr);
 }
 
-function capturarDatosPaciente(form){
+function capturarDatosPaciente(form) {
     var paciente = {
         nombre: form.nombre.value,
         peso: form.peso.value,
         altura: form.altura.value,
         gordura: form.gordura.value,
-        imc: calcularIMC(form.peso.value,form.altura.value)
+        imc: calcularIMC(form.peso.value, form.altura.value)
     }
     return paciente;
 }
 
-function construirTr(paciente){
-        var pacienteTr = document.createElement("tr");
-        pacienteTr.classList.add("paciente");
+function construirTr(paciente) {
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
 
-        pacienteTr.appendChild(construirTd(paciente.nombre, "info-nombre"));
-        pacienteTr.appendChild(construirTd(paciente.peso, "info-peso"));
-        pacienteTr.appendChild(construirTd(paciente.altura, "info-altura"));
-        pacienteTr.appendChild(construirTd(paciente.gordura, "info-gordura"));
-        pacienteTr.appendChild(construirTd(paciente.imc, "info-imc"));
+    pacienteTr.appendChild(construirTd(paciente.nombre, "info-nombre"));
+    pacienteTr.appendChild(construirTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(construirTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(construirTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(construirTd(paciente.imc, "info-imc"));
 
-        return pacienteTr;
+    return pacienteTr;
 }
 
-function construirTd(dato, clase){
+function construirTd(dato, clase) {
     var td = document.createElement("td");
     td.classList.add(clase);
     td.textContent = dato;
     return td;
 }
 
-function validarPaciente(paciente){
+function validarPaciente(paciente) {
     var errores = []
 
-    if (paciente.nombre.length == 0){
+    if (paciente.nombre.length == 0) {
         errores.push("El nombre no puede estar vacio");
     }
 
-    if (paciente.peso.length == 0){
+    if (paciente.peso.length == 0) {
         errores.push("El peso no puede estar vacio");
     }
 
-    if (paciente.altura.length == 0){
+    if (paciente.altura.length == 0) {
         errores.push("La altura no puede estar vacia");
     }
 
-    if (paciente.gordura.length == 0){
+    if (paciente.gordura.length == 0) {
         errores.push("El %gordura no puede estar vacio");
     }
 
-    if (!validarPeso(paciente.peso)){
+    if (!validarPeso(paciente.peso)) {
         errores.push("El peso es incorrecto");
     }
 
-    if (!validarAltura(paciente.altura)){
+    if (!validarAltura(paciente.altura)) {
         errores.push("La altura es incorrecta");
     }
-    
+
     return errores;
 }
 
-function exhibirMensajesErrores(errores){
+function exhibirMensajesErrores(errores) {
     var ul = document.querySelector("#mensajes-errores");
     ul.innerHTML = "";
-    errores.forEach(function(error){
+    errores.forEach(function (error) {
         var li = document.createElement("li");
         li.textContent = error
         ul.appendChild(li);
